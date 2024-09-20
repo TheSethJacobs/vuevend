e24d67ba9fd391f7560ab3fea9b24b134fdf1759
Seth Jacobs <thesethjacobs@gmail.com>
2024-09-20 11:41:49 -0400

    First

---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ItemComponent
+    v-for="item in $store.state.products"
+    v-bind:key="item.id"
+    v-bind:item="item"
+    ></ItemComponent>
+  </div>
+</template>
+
+<script>
+import ItemComponent from './ItemComponent.vue';
+
+export default {
+  components: {ItemComponent}
+}
+</script>
+
+<style>
+
+</style>
